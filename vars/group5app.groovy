d3efa361856076707a8011f3204b036fa06da6c6
TundeO <111096238+tunde-me@users.noreply.github.com>
2022-09-25 23:51:18 -0500

    Update group5app.groovy

--- vars/group5app.groovy
+++ vars/group5app.groovy
@@ -1,42 +1,27 @@
 def call(String repoUrl){
-pipeline{
-    agent any
-    stages{
-        stage('system statistics and Jenkins status check'){
-            parallel{
-                stage('Max'){
-                    steps{
-                        sh "lscpu"
-                         sh "sudo systemctl status jenkins"
-                    }
-                }
-                stage('Chima'){
-                    steps{
-                        sh "lscpu"
-                        sh "sudo systemctl status jenkins"
-                    }
-                }
-            }
-            parallel{
-                stage('Christiana'){
-                    steps{
-                        sh "lscpu"
-                         sh "sudo systemctl status jenkins"
-                    }
-                }
-                stage('Tunde'){
-                    steps{
-                        sh "lscpu"
-                        sh "sudo systemctl status jenkins"
-                    }
-                }
-            }
-        }
-        stage('End of parallel build'){
-            steps{
-                echo "End of pipeline"
-            }
-        }
-    }
+pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
 }
 }
